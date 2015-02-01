5dec7e3a0066398e74ae6bba93fac7bd8d0b4363
VIKTOR <vikt.gustafsson@gmail.com>
2015-02-01 13:46:02 +0100

    lab2 start

--- lab2.py
+++ lab2.py
@@ -3,10 +3,46 @@ class ListQ:
         self.items = []
 
     def isEmpty(self):
-        return self.items == []
+        if self.items == []:
+            return True
+        else:
+            return False
 
     def put(self, item):
-        self.items.insert(0,item)
+        self.items.insert(-1,item)
 
     def get(self):
-        return self.items.pop()
+        return self.items.pop(0)
+        
+"""class Sort(ListQ):
+    def __init__(self, unsortedList):
+        self.unsortedList = unsortedList
+    def magi(unsortedList):
+       # [3 1 4 2 5] [12 1 8 2 10 3 7 4 9 5 11 6 13]
+        unsortedList.put()
+        unsortedList[0] = get()"""
+        
+    
+def sort(x):
+    
+
+def main():
+    question = input("Du har 5 kort. I vilken ordning vill du laegga dessa?")
+    numbers = question.split()
+    x = ListQ()
+    for i in numbers:
+        x.put(i)
+    print(x.isEmpty())
+    d  = Sort(x)
+    print(d.magi())
+  
+  #  print("De kommer ut i ordningen: " + x[0] + x[1] + x[2] + x[3] +)
+#if __name__== "main":
+    #Skapar en queue, init säger att den ska skapas tom. isEmpty kollar att den är tom.
+    #q = ListQ()
+    #  if q.isEmpty():
+     #     print("q.isEmpty() ger raett svar.")
+     # else:
+     #     print("q.isEmpty() ger FEL svar".)
+main()
+    
