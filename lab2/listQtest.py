ceb9a638df9461e1c1aee2b45837568cecab41fc
mightyGurar <gustavhoglund@hotmail.com>
2015-02-13 16:52:35 +0100

    linkQtest klar

--- lab2/listQtest.py
+++ lab2/listQtest.py
@@ -30,3 +30,13 @@ if q.isEmpty():
     print("q.isEmpty() ger raett svar.\n")
 else:
     print("q.isEmpty() ger FEL svar.\n")
+
+
+numbers = [1,2,3]
+print("***** TEST 4 *****")
+print('Forvaentar raett svar\n')
+x = Sort(numbers)
+if str(x) == '2 1 3 ':
+	print('q.magicSort() ger raett svar.\n')
+else:
+	print('q.magicSort() ger FEL svar.\n')
--- lab2/sortFile.py
+++ lab2/sortFile.py
@@ -5,6 +5,13 @@ class Sort(ListQ): #Aerver metoder fran listQ-klassen
         self.items = unsortedList
         self.sortedList = []
         self.magicSort()
+
+    def __str__(self):
+    	s = ''
+    	for n in self.sortedList:
+    		s = s + str(n) + ' '
+    	return s
+
         
     def magicSort(self): #Metod som laegger till varannat element langst bak i kon och naestkommande till en "sorterad lista".
         while not self.isEmpty():
