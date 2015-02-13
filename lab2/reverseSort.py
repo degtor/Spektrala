581827cfd650469066b21805684fca17a6131730
mightyGurar <gustavhoglund@hotmail.com>
2015-02-13 17:03:35 +0100

    fixat reverseSort for presenteation

--- lab2/reverseSort.py
+++ lab2/reverseSort.py
@@ -1,2 +1,19 @@
-for a in range(5):
-	print(a)
+from linkedQfile import LinkedQ
+
+def main():
+	linkA = LinkedQ()
+	linkB = LinkedQ()
+
+	antal_kort = input('Hur många kort vill du sortera? ')
+
+	for n in range(1,int(antal_kort) + 1):
+		linkA.putFirst(n)
+
+	while not linkA.isEmpty():
+		linkB.putFirst(linkA.getFirst())
+		linkB.putFirst(linkB.last)
+		linkB.removeLast()
+
+	return linkB
+
+print(main())
