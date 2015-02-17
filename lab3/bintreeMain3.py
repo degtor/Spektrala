35d2aea9ea60bdeff8a3927108654a26aee79fdd
mightyGurar <gustavhoglund@hotmail.com>
2015-02-17 16:37:30 +0100

    uppg 1-3 klar ej kommenterade

--- lab3/bintreeMain3.py
+++ lab3/bintreeMain3.py
@@ -3,3 +3,23 @@ from bintreeFile import Bintree
 def main():
 	svenska = Bintree()
 	engelska = Bintree()
+
+	with open('word3.txt', 'r', encoding = 'utf-8') as svenskfil:
+		for rad in svenskfil:
+			ordet = rad.strip()
+			if not svenska.exists(ordet):
+				svenska.put(ordet)
+
+	with open('engelska.txt', 'r', encoding = 'utf-8') as engelskfil:
+		for rad in engelskfil:
+			ordrad = rad.split()
+			for ordet in ordrad:
+				if engelska.exists(ordet):
+					pass
+				else:
+					engelska.put(ordet)
+					if svenska.exists(ordet):
+						print(ordet, end = ' ')
+	print('\n')
+
+main()
